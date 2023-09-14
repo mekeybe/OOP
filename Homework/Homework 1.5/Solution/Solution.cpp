000957b359ab98ca468458babe3d776bcd991f4f
mekeybe <127078178+mekeybe@users.noreply.github.com>
2023-09-14 12:06:14 +0300

    Add files via upload

---
+++ Homework/Homework 1.5/Solution/Solution.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+struct Book
+{
+    char title[100];
+    int page;
+    float price;
+};
+
+void addPrice(struct Book& A, float x)
+{
+    A.price += x;
+    std::cout << "Price after changing: " << A.price << endl;
+}
+
+int main()
+{
+    float x;
+    Book A = { "The Tale", 230, 245.2};
+    std::cout << "Input number: ";
+    cin >> x;
+    std::cout << "Price before changing: " << A.price << endl;
+    addPrice(A, x);
+}
