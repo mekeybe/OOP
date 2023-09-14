ead95bcb26bad1b017d4dac8f21d21af99df00bd
mekeybe <127078178+mekeybe@users.noreply.github.com>
2023-09-14 10:01:32 +0300

    Add files via upload

---
+++ Homework/Homework 1.1/Solution/Solution.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+namespace myspace
+{
+    void printNTimes(const char* str, unsigned int n)
+    {
+        ostringstream repeated;
+        fill_n(ostream_iterator<string>(repeated), n, str);
+        cout << "Output: " << repeated.str() << endl;
+    }
+}
+
+using myspace::printNTimes;
+
+int main()
+{
+    string str;
+    unsigned int n;
+    cout << "Input your line and the number of repetitions: ";
+    cin >> str >> n;
+    printNTimes(&str[0], n);
+}
